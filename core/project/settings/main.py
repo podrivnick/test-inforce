86204b6fad7b27290cd27ae1914a2e6126663342
Rybakov Artem Oleekseevich <vp41919@gmail.com>
2025-02-18 13:21:59 +0200

    fix: automatic secret key generation

--- core/project/settings/main.py
+++ core/project/settings/main.py
@@ -13,6 +13,8 @@ import os.path
 from datetime import timedelta
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 import environ
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
@@ -28,7 +30,8 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
